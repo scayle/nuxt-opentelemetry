02a5e86b7f34e46756f28e7d30e51f82dc004c65
Robert K. Pauls <robert.pauls@scayle.com>
2025-05-28 15:22:41 +0200

    [BACKPORT] Add missing dependencies, tweaked vitest setup and fixup package.json files

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,18 @@
 export default defineNuxtConfig({
+  // https://nuxt.com/docs/api/nuxt-config#compatibilitydate
+  compatibilityDate: '2024-09-03',
+
+  // https://nuxt.com/docs/api/nuxt-config#devtools
+  devtools: { enabled: false },
+
+  // https://nuxt.com/docs/api/nuxt-config#debug
+  debug: false,
+
+  // https://nuxt.com/docs/api/nuxt-config#modules-1
   modules: ['../src/module'],
+
+  // OpenTelemetry
   opentelemetry: {
     enabled: true,
   },
-  devtools: { enabled: true },
 })
